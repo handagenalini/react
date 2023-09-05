befb246d605fd5946458105c1e78fe1e05c9083c
handagenalini <handagenalini@gmail.com>
2023-09-05 14:28:01 +0530

    added bootstrap

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Expenceitem from './components/expenceitem';
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenceitem></Expenceitem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/expenceitem.js
@@ -0,0 +1,8 @@
+function expenceitem(){
+    return <div><h2> expence item!</h2>
+            <h2>Food Rs 10</h2>
+            <h2>Petrol Rs 100</h2>
+            <h2>Movies Rs 200</h2>
+            </div>
+}
+export default expenceitem;
