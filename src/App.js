7ab594bef229fa2ca42ad28df4c39b3d279b8107
handagenalini <handagenalini@gmail.com>
2023-09-05 20:23:56 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import Expenceitem from './components/expenceitem';
+import ExpenseItem from './components/expenceitem';
 function App() {
+  const expenses=[
+    {
+      title:"Food",
+      price:500,
+      date:new Date(2023,7,21)
+    },
+    {
+      title:"House",
+      price:50000,
+      date:new Date(2023,7,21)
+    },{
+      title:"Car",
+      price:5000,
+      date:new Date(2023,7,21)
+    },{
+      title:"Bike",
+      price:500000,
+      date:new Date(2023,7,21)
+    },
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenceitem></Expenceitem>
+     
+      <ExpenseItem title={expenses[0].title} price={expenses[0].price} date={expenses[0].date} ></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} price={expenses[1].price} date={expenses[1].date} ></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} price={expenses[2].price} date={expenses[2].date} ></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} price={expenses[3].price} date={expenses[3].date} ></ExpenseItem>
     </div>
   );
 }
--- src/components/expenceitem.js
+++ src/components/expenceitem.js
@@ -1,16 +1,16 @@
 import './expenseitem.css';
-function expenceitem(){
-    const expensedate=new Date(2023,6,22);
-    console.log(expensedate)
-    const expensetitle="Food"
-    const expenseAmount=100
+function expenceitem(props){
+  
     return (<div className='expense-item'>
-        <div>{expensedate.toISOString()}</div>
-        <div className='expense-item__description'>
-            <h2>{expensetitle}</h2>
-            <div className='expense-item__price'>${expenseAmount}</div>
-          
-            </div>
+     
+    
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        
+     
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>Rs.{props.price}</div>
+        </div>
             </div>)
             }
 export default expenceitem;
